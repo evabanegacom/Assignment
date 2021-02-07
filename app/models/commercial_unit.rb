39452be1fa9b06de0956fbe418feb3f6f617c6f4
Precious chiedu <udegbue69@gmail.com>
2021-02-07 12:07:47 +0100

    added stripe payment implementation

--- app/models/commercial_unit.rb
+++ app/models/commercial_unit.rb
@@ -3,4 +3,5 @@
 class CommercialUnit < ApplicationRecord
   belongs_to :manager, class_name: 'User', foreign_key: :user_id
   has_one_attached :icon
+  validates :price, presence: true, length: { minimum: 3 }
 end
